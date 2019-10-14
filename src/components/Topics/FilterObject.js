89c031cc4d8884c9fc8c6143d71f9be9c5b8eb50
Mimi <mimitraugott@gmail.com>
2019-10-14 00:07:26 -0600

    push

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,10 +1,64 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class FilterObject extends Component {
-    render () {
+    constructor() {
+        super();
+
+        this.state = {
+            students: [
+                {
+                    name: 'Peter Smith',
+                    title: 'Senior Student',
+                    age: 39,
+                },
+                {
+                    name: 'Sara Anderson',
+                    age: 24,
+                    hobby: 'guitar',
+                },
+                {
+                    name: 'Steve Carlson',
+                    title: 'Sophmore Student',
+                },
+            ],
+
+            userInput: '',
+            filteredStudents: [],
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val });
+    }
+
+    filterStudents(prop) {
+        let students = this.state.students;
+        let filterStudents = [];
+
+        for (let i = 0; i < students.length; i++) {
+            if (students[i].hasOwnProperty(prop)) {
+                filterStudents.push(students[i]);
+            }
+        }
+        this.setState({ filterStudents: filterStudents });
+    }
+
+    render() {
         return (
-        <p> FilterObject Component </p>
+            <div className="puzzleBox FilterObjectPB">
+
+                <h4> Filter Object </h4>
+
+                <span className="puzzleText"> Original: {JSON.stringify(this.state.students, null, 10)}</span>
+
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)} ></input>
+
+                <button className="confirmationButton" onClick={() => this.filterStudents(this.state.userInput)}> Filter </button>
+                <span className="resultsBox"> Filtered: {JSON.stringify(this.state.filterStudents, null, 10)}</span>
+&nbsp;
+            </div>
         )
     }
+
 }
 export default FilterObject
